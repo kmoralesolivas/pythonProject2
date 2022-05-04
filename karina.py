577c48829a7fc7edeec54e0a5f4575f0479f122c
kmoralesolivas <103004500+kmoralesolivas@users.noreply.github.com>
2022-05-04 17:23:37 -0600

    Create karina.py

---
+++ karina.py
@@ -0,0 +1,17 @@
+import streamlit as st
+st.cache
+def run_fxn(n: int) -> list:
+    return range(n)
+def main():
+    """Generación de la webapp con streamlit"""
+    
+    st.title( "MICROBIOLOGIA")
+    st.header("LEVADURAS")
+    st.subheader("Buttons")
+    if st.button("Que Son?"):
+        st.text("Son hongos microscópicos, habitualmente unicelululares")
+")
+    else:
+        st.text("")
+    st.subheader("FALSAS LEVADURAS")
+    
