1d93d0322d14dd7c39dfe98b861e77ddea71e15e
kmoralesolivas <103004500+kmoralesolivas@users.noreply.github.com>
2022-05-04 17:00:18 -0600

    Update Interfaz 2.0.py

--- Interfaz 2.0.py
+++ Interfaz 2.0.py
@@ -8,8 +8,9 @@ def main():
     st.title( "MICROBIOLOGIA")
     st.header("LEVADURAS")
     st.subheader("Buttons")
-    if st.button("QUE SON"):
-        st.text("Streamlit es Cool")
+    if st.button("Que Son?"):
+        st.text("Son hongos microscópicos, habitualmente unicelulares. Aunque la aparición de hongos en nuestros productos alimenticios generalmente nos hace pensar que no es bueno,las levaduras, se llevan usando de forma controlada miles de años
+")
     else:
         st.text("")
     st.subheader("FALSAS LEVADURAS")
