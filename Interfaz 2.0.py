44b2ae3c45b3b008b1949628b63d23ba44b32a3c
kmoralesolivas <103004500+kmoralesolivas@users.noreply.github.com>
2022-05-04 16:47:55 -0600

    Update Interfaz 2.0.py

--- Interfaz 2.0.py
+++ Interfaz 2.0.py
@@ -2,3 +2,8 @@ import streamlit as st
 st.cache
 def run_fxn(n: int) -> list:
     return range(n)
+def main():
+    """Generación de la webapp con streamlit"""
+    # Definir título
+    st.title( "MICROBIOLOGIA")
+    
